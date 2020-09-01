b58ac436af0bc98c362a64e7a7076a0f53cbfef7
Амелин Евгений <amelin_ev@astral.ru>
2020-09-02 02:23:56 +0300

    Боже, очредной фикс одной и той же фигни. Надеюсь, никто этого не увидит. Не показывайте детям

--- terminal.go
+++ terminal.go
@@ -27,7 +27,7 @@ func ColorPrint(val interface{}, foreground bool, background bool, color ...int)
 			fmt.Print(val)
 			return
 		}
-		fmt.Println(fmt.Sprintf("\033[38;2;%d;%d;%dm\033[48;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], color[3], color[4], color[5], val))
+		fmt.Print(fmt.Sprintf("\033[38;2;%d;%d;%dm\033[48;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], color[3], color[4], color[5], val))
 		return
 	}
 
