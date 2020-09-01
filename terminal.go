7f37df85b608016b3f81383ebbfea2860b0cca34
Амелин Евгений <amelin_ev@astral.ru>
2020-09-02 01:10:00 +0300

    Добавил метод для вывода цветного текста в терминал

---
+++ terminal.go
@@ -0,0 +1,45 @@
+package gotools
+
+import "fmt"
+
+// Цветной вывод текста в терминал
+func ColorPrint(val interface{}, foreground bool, background bool, color ...int) {
+	// Без цвета
+	if !foreground && !background {
+		fmt.Print(val)
+		return
+	}
+
+	// Цвет текста и фона
+	if foreground && background {
+		if len(color) < 6 {
+			fmt.Print(val)
+			return
+		}
+		fmt.Println(fmt.Sprintf("\033[38;2;%d;%d;%dm\033[48;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], color[3], color[4], color[5], val))
+	}
+
+	// Только цвет текста
+	if foreground {
+		if len(color) < 3 {
+			fmt.Println(val)
+			return
+		}
+		fmt.Println(fmt.Sprintf("\033[38;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], val))
+	}
+
+	// Только цвет фона
+	if background {
+		if len(color) < 3 {
+			fmt.Println(val)
+			return
+		}
+		fmt.Println(fmt.Sprintf("\033[48;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], val))
+	}
+}
+
+// Цветной вывод текста в терминал с переносом на новую строку
+func ColorPrintln(val interface{}, foreground bool, background bool, color ...int) {
+	ColorPrint(val, foreground, background, color...)
+	fmt.Println()
+}
