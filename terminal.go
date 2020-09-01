f96c8b16a390d7678dc0e43dffec3f3c95fd8a2f
Амелин Евгений <amelin_ev@astral.ru>
2020-09-02 01:25:25 +0300

    Маленький фикс цветного вывода в терминал

--- terminal.go
+++ terminal.go
@@ -17,6 +17,7 @@ func ColorPrint(val interface{}, foreground bool, background bool, color ...int)
 			return
 		}
 		fmt.Println(fmt.Sprintf("\033[38;2;%d;%d;%dm\033[48;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], color[3], color[4], color[5], val))
+		return
 	}
 
 	// Только цвет текста
@@ -26,6 +27,7 @@ func ColorPrint(val interface{}, foreground bool, background bool, color ...int)
 			return
 		}
 		fmt.Println(fmt.Sprintf("\033[38;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], val))
+		return
 	}
 
 	// Только цвет фона
@@ -35,6 +37,7 @@ func ColorPrint(val interface{}, foreground bool, background bool, color ...int)
 			return
 		}
 		fmt.Println(fmt.Sprintf("\033[48;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], val))
+		return
 	}
 }
 
