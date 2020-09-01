25e924d4c36b3465a42eba7e73188084ed94a312
Амелин Евгений <amelin_ev@astral.ru>
2020-09-02 02:18:26 +0300

    Фиксы цветного вывода и добавлен набор стандартных цветов

--- terminal.go
+++ terminal.go
@@ -2,6 +2,17 @@ package gotools
 
 import "fmt"
 
+// Цвета, которые можно использовать для вывода в терминал За основу взяты цвета JetBrains Darcula Theme.
+// Например: Цвет текста и фона: ColorPrintln("text", true, true, append(Red, Black...)...)
+var (
+	Red    = []int{210, 82, 82}
+	Yellow = []int{255, 198, 109}
+	Green  = []int{97, 150, 71}
+	Blue   = []int{104, 151, 187}
+	White  = []int{255, 255, 255}
+	Black  = []int{0, 0, 0}
+)
+
 // Цветной вывод текста в терминал
 func ColorPrint(val interface{}, foreground bool, background bool, color ...int) {
 	// Без цвета
@@ -23,20 +34,20 @@ func ColorPrint(val interface{}, foreground bool, background bool, color ...int)
 	// Только цвет текста
 	if foreground {
 		if len(color) < 3 {
-			fmt.Println(val)
+			fmt.Print(val)
 			return
 		}
-		fmt.Println(fmt.Sprintf("\033[38;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], val))
+		fmt.Print(fmt.Sprintf("\033[38;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], val))
 		return
 	}
 
 	// Только цвет фона
 	if background {
 		if len(color) < 3 {
-			fmt.Println(val)
+			fmt.Print(val)
 			return
 		}
-		fmt.Println(fmt.Sprintf("\033[48;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], val))
+		fmt.Print(fmt.Sprintf("\033[48;2;%d;%d;%dm%v\033[0m", color[0], color[1], color[2], val))
 		return
 	}
 }
